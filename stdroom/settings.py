2d8f393e77303b9df12c58eabbd69c30043fe906
YUNJI JANG <bee1513@gmail.com>
2019-07-11 16:46:10 +0900

    collectstatic

--- stdroom/settings.py
+++ stdroom/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'stdroomres/static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'stdroomres/static')]
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
